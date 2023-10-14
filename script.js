8dc4eea80ce91a62b8053120438581ccd37aa206
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2023-10-15 00:34:06 +0200

    feat: add magnetic-btn

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ const navItemAnimation = () => {
 }
 navItemAnimation()
 
+const magneticBtn = () => {
+    const btn = document.querySelector(".magnetic-btn");
+
+    btn.addEventListener("mousemove", function (e) {
+        const position = btn.getBoundingClientRect();
+        const x = e.pageX - position.left - position.width / 2;
+        const y = e.pageY - position.top - position.height / 2;
+
+        btn.children[0].style.transform =
+            "translate(" + x * 0.3 + "px, " + y * 0.5 + "px)";
+    });
+
+    btn.addEventListener("mouseout", function (e) {
+        btn.children[0].style.transform = "translate(0px, 0px)";
+    });
+}
+magneticBtn()
